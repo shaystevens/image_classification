ca8ace1ee5661b6f82c265adfec1a89b52527f7b
shaystevens <shaystevns@gmail.com>
2023-07-01 16:56:14 +1200

    Code commented

--- image_classification.py
+++ image_classification.py
@@ -1,44 +1,57 @@
+# Import the necessary modules
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, layers, models
 
-
+# Load the CIFAR-10 dataset. It returns two tuples of Numpy arrays. 
+# The first tuple represents the training dataset and the second one the test dataset.
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+
+# Normalize the pixel values of the images from [0, 255] to [0, 1] for better performance 
 training_images, testing_images = training_images / 255.0, testing_images / 255.0
 
+# Define the names for the classes
 class_names =['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
 
+# Plot first 16 images from the training set
 for i in range(16):
-    plt.subplot(4, 4, i + 1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.imshow(training_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[training_labels[i][0]])
+    plt.subplot(4, 4, i + 1)  # We'll have 4 rows and 4 columns of images
+    plt.xticks([])  # Remove x-axis markers
+    plt.yticks([])  # Remove y-axis markers
+    plt.imshow(training_images[i], cmap=plt.cm.binary)  # Display the image in black and white
+    plt.xlabel(class_names[training_labels[i][0]])  # Label the image with its class name
 
-plt.show()
+plt.show()  # Display the plot
 
+# Trim the training and testing datasets to reduce computation time
 training_images = training_images[:40000]
 training_labels = training_labels[:40000]
 testing_images = testing_images[:8000]
 testing_labels = testing_labels[:8000]
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
+# Build the model
+model = models.Sequential()  # Sequential model is a linear stack of layers
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))  # First convolutional layer
+model.add(layers.MaxPooling2D((2,2)))  # First pooling layer
+model.add(layers.Conv2D(64, (3,3), activation='relu'))  # Second convolutional layer
+model.add(layers.MaxPooling2D((2,2)))  # Second pooling layer
+model.add(layers.Conv2D(64, (3,3), activation='relu'))  # Third convolutional layer
+model.add(layers.Flatten())  # Flatten layer to convert the 3D data to 1D
+model.add(layers.Dense(64, activation='relu'))  # Dense layer for performing the classification
+model.add(layers.Dense(10, activation='softmax'))  # Output layer. The softmax function is used for multi-class classification
 
+# Compile the model
 model.compile(optimizer="adam", loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 
+# Train the model
 model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
 
+# Evaluate the model performance
 loss, accuracy = model.evaluate(testing_images, testing_labels)
-print(f":Loss: {loss}")
-print(f"Accuracy: {accuracy}")
+print(f":Loss: {loss}")  # Print the loss value
+print(f"Accuracy: {accuracy}")  # Print the accuracy value
 
+# Save the model for later use
 model.save("image_classifier.model")
+
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras import datasets, layers, models
+# Import necessary libraries
+import cv2 as cv # OpenCV for image processing
+import numpy as np # NumPy for numerical computations
+import matplotlib.pyplot as plt # Matplotlib for plotting
+from tensorflow.keras import datasets, layers, models # TensorFlow and Keras for machine learning
 
+# Define the class names. These are the categories that the model has been trained to recognize.
 class_names =['Plane', 'Car', 'Bird', 'Car', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
 
+# Load the previously trained model from a file
 model = models.load_model("image_classifier.model")
 
-img = cv.imread("IMAGE.JPEG") # Add classification image here
+# Load the image to be classified
+img = cv.imread("IMAGE.JPEG")
+
+# Convert the image from BGR color format (which OpenCV uses) to RGB format
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+# Resize the image to the size that the model expects (32x32 pixels in this case)
 resized_img = cv.resize(img, (32, 32)) 
 
+# Display the resized image using Matplotlib
 plt.imshow(resized_img, cmap=plt.cm.binary)
 plt.show()
 
+# Use the model to predict the class of the image. The image is first converted to an array,
+# and the pixel intensities are normalized to be between 0 and 1 by dividing by 255.
 prediction = model.predict(np.array([resized_img]) / 255)
+
+# The model's prediction is an array of probabilities for each class. We take the index of 
+# the highest probability using np.argmax, which gives us the predicted class.
 index = np.argmax(prediction)
+
+# Finally, we print the name of the predicted class
 print(f'Prediction is {class_names[index]}')
