07202c23c59f2b384f97bc91c4847822ca972eae
shaystevens <shaystevns@gmail.com>
2023-07-01 14:02:22 +1200

    Model trained

--- image_classification.py
+++ image_classification.py
@@ -18,5 +18,21 @@ for i in range(16):
 
 plt.show()
 
+training_images = training_images[:20000]
+training_labels = training_labels[:20000]
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
 
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
 
+model.compile(optimizer="adam", loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
